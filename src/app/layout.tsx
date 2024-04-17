c795738be8067aa141e6b7de6b48282b9a64b8d3
3mpty <aurelien.corre@outlook.fr>
2024-04-17 23:54:18 +0200

    Added stuff in head in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,19 +25,6 @@ export const metadata: Metadata = {
     formatDetection: {
         telephone: false,
     },
-    openGraph: {
-        type: "website",
-        siteName: APP_NAME,
-        title: APP_NAME,
-        description: APP_DESCRIPTION,
-        images: IMAGE_URL,
-    },
-    twitter: {
-        card: "summary",
-        title: APP_NAME,
-        description: APP_DESCRIPTION,
-        images: IMAGE_URL,
-    },
 };
 
 export const viewport: Viewport = {
@@ -53,6 +40,33 @@ export default function RootLayout({
         <html lang="fr">
             <head>
                 <link rel="manifest" href="/manifest.json" />
+                <meta property="og:title" content="Raptest" />
+                <meta property="og:site_name" content="Raptest" />
+                <meta property="og:url" content="https://raptest.vercel.app" />
+                <meta
+                    property="og:description"
+                    content="Test tes connaissances des textes de tes rappeurs préférés avec ce blindtest revisité !"
+                />
+                <meta property="og:type" content="website" />
+                <meta
+                    property="og:image"
+                    content="https://ogcdn.net/c078a98b-9a33-4eaf-a5cf-e5ebf3ea450c/v1/raptest.vercel.app/Raptest/Test%20tes%20connaissances%20des%20textes%20de%20tes%20rappeurs%20pr%C3%A9f%C3%A9r%C3%A9s%20avec%20ce%20blindtest%20revisit%C3%A9%20!/https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fdocuments%2Ff2f67fad-38cf-4ed7-8a03-64582a2eb9c0.jpg%3Ftoken%3DKfG5r6wVrp4KWgeiqnEdPxLH6d5DRapjqO6vZlSG8uM%26height%3D675%26width%3D1200%26expires%3D33249390372/og.png"
+                />
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta property="twitter:domain" content="raptest.vercel.app" />
+                <meta
+                    property="twitter:url"
+                    content="https://www.raptest.vercel.app"
+                />
+                <meta name="twitter:title" content="Raptest" />
+                <meta
+                    name="twitter:description"
+                    content="Test tes connaissances des textes de tes rappeurs préférés avec ce blindtest revisité !"
+                />
+                <meta
+                    name="twitter:image"
+                    content="https://ogcdn.net/c078a98b-9a33-4eaf-a5cf-e5ebf3ea450c/v1/raptest.vercel.app/Raptest/Test%20tes%20connaissances%20des%20textes%20de%20tes%20rappeurs%20pr%C3%A9f%C3%A9r%C3%A9s%20avec%20ce%20blindtest%20revisit%C3%A9%20!/https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fdocuments%2Ff2f67fad-38cf-4ed7-8a03-64582a2eb9c0.jpg%3Ftoken%3DKfG5r6wVrp4KWgeiqnEdPxLH6d5DRapjqO6vZlSG8uM%26height%3D675%26width%3D1200%26expires%3D33249390372/og.png"
+                />
             </head>
             <body>
                 <header>
