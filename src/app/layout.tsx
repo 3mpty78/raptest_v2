b4d709f9127a9f62ee586d6f32c2666466b851ec
3mpty <aurelien.corre@outlook.fr>
2024-04-17 23:40:18 +0200

    Updated METADATA in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,8 @@ const APP_NAME = "Raptest";
 const APP_DESCRIPTION =
     "Test tes connaissances des textes de tes rappeurs préférés avec ce blindtest revisité !";
 const APP_TITLE_TEMPLATE = "%s - Raptest";
+const IMAGE_URL =
+    "https://ogcdn.net/2c2c6737-47d4-4459-9969-e711eb48394c/v1/raptest.vercel.app/Raptest/Test%20tes%20connaissances%20des%20textes%20de%20tes%20rappeurs%20pr%C3%A9f%C3%A9r%C3%A9s%20avec%20ce%20blindtest%20revisit%C3%A9%20!/https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fdocuments%2Fecd174a6-123a-4487-9f72-2c702d4ec70c.jpg%3Ftoken%3D7iLDyNXzAUtcdo-aRJdldHW-CPKcfF_lHfwf6yQA3ec%26height%3D800%26width%3D1200%26expires%3D33249389526/og.png";
 
 export const metadata: Metadata = {
     applicationName: APP_NAME,
@@ -26,19 +28,15 @@ export const metadata: Metadata = {
     openGraph: {
         type: "website",
         siteName: APP_NAME,
-        title: {
-            default: APP_NAME,
-            template: APP_TITLE_TEMPLATE,
-        },
+        title: APP_NAME,
         description: APP_DESCRIPTION,
+        images: IMAGE_URL,
     },
     twitter: {
         card: "summary",
-        title: {
-            default: APP_NAME,
-            template: APP_TITLE_TEMPLATE,
-        },
+        title: APP_NAME,
         description: APP_DESCRIPTION,
+        images: IMAGE_URL,
     },
 };
 
